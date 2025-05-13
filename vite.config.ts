1a9e9b1adcd6c94efe5e2f6fc02df2013a506dec
Juan Huamani <jhuamaniv@unsa.edu.pe>
2025-05-12 22:48:35 -0500

    Eliminar la configuración de salida de archivos en vite.config.ts y ajustar la propiedad "base" para mantener la consistencia en la configuración del portafolio.

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio',
+  base: "https://juanhuamani.github.io/Portfolio",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
 })
