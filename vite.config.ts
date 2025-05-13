130c3c2e7e74952062529554675eb616445230e8
Juan Huamani <jhuamaniv@unsa.edu.pe>
2025-05-12 22:55:26 -0500

    Actualizar la propiedad "base" en vite.config.ts para incluir una barra al final de la URL del portafolio, asegurando consistencia en la configuración.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://juanhuamani.github.io/Portfolio",
+  base: "https://juanhuamani.github.io/Portfolio/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
